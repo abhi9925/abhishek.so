bbf9a4607d20cea4b01cb6dc2ba489d8cc8ca349
abhi9925 <30955441+abhi9925@users.noreply.github.com>
2022-02-25 23:05:27 +0530

    v1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fleerob%2Fleerob.io)
 
-# leerob.io
+# abhishek.so
 
 - **Framework**: [Next.js](https://nextjs.org/)
 - **Database**: [PlanetScale](https://planetscale.com)
--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ import avatarBW from 'public/avatar-bw.jpg';
 
 export default function About() {
   return (
-    <Container title="About – Lee Robinson">
+    <Container title="About – Abhishek Jain">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
@@ -16,27 +16,31 @@ export default function About() {
           <h2>Links</h2>
           <ul>
             <li>
-              Twitter: <a href="https://twitter.com/leeerob">@leeerob</a>
+              Twitter: <a href="https://twitter.com/abhishekjain25">@abhishekjain25</a>
             </li>
-            <li>
+            {/* <li>
               GitHub: <a href="https://github.com/leerob">@leerob</a>
-            </li>
+            </li> */}
             <li>
               Website:{' '}
-              <Link href="https://leerob.io">
-                <a>https://leerob.io</a>
+              <Link href="https://abhishek.so">
+                <a>https://abhishek.so</a>
               </Link>
             </li>
             <li>
               LinkedIn:{' '}
-              <a href="https://www.linkedin.com/in/leeerob/">
-                https://www.linkedin.com/in/leeerob
+              <a href="https://www.linkedin.com/in/abhishek-jain9925/">
+                https://www.linkedin.com/in/abhishek-jain9925
               </a>
             </li>
           </ul>
           <h2>Bio</h2>
-          <h3>Job Title</h3>
-          <p>Lee Robinson, Director of Developer Relations at Vercel</p>
+          <h3>Co-founder & CEO</h3>
+          <p><a href="http://telematica.so/">Telematica</a></p>
+          <p> - Telematica API enables businesses to easily access their users' Electric Vehicle data and build powerful green apps <br>
+          </br> - Backed by Y Combinator W22 batch
+           </p>
+
           <h3>Long, 3rd Person</h3>
           <p>
             Lee Robinson is the Director of Developer Relations at{' '}
@@ -69,8 +73,8 @@ export default function About() {
           </p>
           <h3>Education</h3>
           <p>
-            Lee Robinson graduated from Iowa State University with a BS in
-            Computer Engineering.
+            Abhishek Jain graduated from BITS Pilani with a BE in
+            Computer Science and MSc in Chemistry.
           </p>
           <h2>Headshots</h2>
           <div className="flex space-x-8">
